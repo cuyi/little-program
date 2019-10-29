7af0718c3032e84feb19505be938b56d873219cd
cuyi <cuyi@qq.com>
2019-10-30 00:13:16 +0800

    add hash init

--- src/word_hash.c
+++ src/word_hash.c
@@ -1,6 +1,51 @@
 #include <stdio.h>
 
-typedef struct {
-    int index;
+#define MAX_WORD_LEN 64
 
-}word_hash_node_t;
+typedef struct word_hash_node{
+    char word[MAX_WORD_LEN];
+    struct word_hash_node *next;
+} word_hash_node_t;
+
+typedef struct word_hash_tbl{
+    word_hash_node_t **head;
+    int bucket_size;
+} word_hash_tbl_t;
+
+word_hash_tbl_t *word_hash_init(int bucket_size)
+{
+    word_hash_tbl_t *tbl;
+    tbl = (word_hash_tbl_t*)malloc(sizeof(word_hash_tbl_t));
+
+    if (tbl == NULL) {
+        return NULL;
+    }
+
+    tbl->bucket_size = bucket_size;
+    tbl->head = (word_hash_node_t*)malloc(sizeof(word_hash_node_t) * bucket_size);
+    if (tbl->head == NULL) {
+        free(tbl);
+        return NULL;
+    }
+
+    for (int i = 0; i < bucket_size; i++) {
+        tbl->head[i] = NULL;
+    }
+
+    return tbl;
+}
+
+int word_get_hash_index(const char* word, int len)
+{
+    int sum = 0;
+    int h = 0;
+    char *p = word;
+
+    while(p - word < len){
+        short a = *(p++)
+        sum ^= a * (p - word);
+        h ^= a / (p - word);
+    }
+
+    return ((sum << 16) | h) % MAX_PRIME_LESS_THAN_BUCKET_SIZE;
+}
